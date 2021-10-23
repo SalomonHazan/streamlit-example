ecafdf8c30b96bf08d5926bdf0873dc38f27925b
Salomon Hazan <48911684+SalomonHazan@users.noreply.github.com>
2021-10-24 00:50:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,12 @@ st.write("I enjoy to discover stremalit possibilities")
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
 df_weather = pd.read_csv(link)
 df_weather
-st.table(df_weather)
 st.dataframe(df_weather, 1000, 200)
 st.line_chart(df_weather['MAX_TEMPERATURE_C'])
 
 import seaborn as sns
-viz_correlation = sns.heatmap(df_weather.corr(), 
-								center=0,
-								cmap = sns.color_palette("vlag", as_cmap=True)
+viz_correlation = sns.heatmap(df_weather.corr(), center=0, cmap = sns.color_palette("vlag", as_cmap=True)
 								)
-
 st.pyplot(viz_correlation.figure)
 
 name = st.text_input("Please give me your name :")
