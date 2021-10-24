553781f0044c8cb2ee1371d8d6c61580a30e3fa5
Salomon Hazan <48911684+SalomonHazan@users.noreply.github.com>
2021-10-24 22:43:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,159 @@
-import streamlit as st
-import pandas as pd
+
+ 
+Pulls
+Issues
+Marketplace
+Explore
+ 
+@SalomonHazan 
+python-engineer
+/
+streamlit-demo
+Public
+ Watch 
+2
+ Star 36
+ Fork 32
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+More
+ master 
+streamlit-demo/main.py  / Jump to 
+Go to file
+
+@Drashti125
+Drashti125 st.pyplot(fig) here we have to pass a parameter
+Latest commit aaf16a8 on 21 Sep 2020
+ History
+ 2 contributors
+@Drashti125 @python-engineer
+109 lines (86 sloc)  2.67 KB
+Raw Blame
+     
+import streamlit as st 
+import numpy as np 
+
+import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-import matplotlib as pl
-import numpy as np
 
-st.title('Machine Learning... The Streamlit Way')
+from sklearn.metrics import accuracy_score
+
+st.title('Streamlit Example')
 
 st.write("""
-# Explorer différents modeles de classification
-Quel est le meilleur modèle ?
+# Explore different classifier and datasets
+Which one is the best?
 """)
 
-dataset_name=st.sidebar.selectbox("Selectionner un Dataset",("Iris", "Breast Cancer", "Wine dataset"))
-st.write(dataset_name)
+dataset_name = st.sidebar.selectbox(
+    'Select Dataset',
+    ('Iris', 'Breast Cancer', 'Wine')
+)
 
-classifier_name=st.sidebar.selectbox("Selectionner un modèle de classification",("KNN", "SVM", "Random Forest"))
+st.write(f"## {dataset_name} Dataset")
 
-def get_dataset(dataset_name):
-	if dataset_name=="Iris":
-		data=datasets.load_iris()
-	elif dataset_name=="Breast Cancer":
-		data=datasets.load_breast_cancer()
-	else:
-		data=datasets.load_wine()
-	X=data.data
-	y=data.target
-	return X, y
+classifier_name = st.sidebar.selectbox(
+    'Select classifier',
+    ('KNN', 'SVM', 'Random Forest')
+)
 
-X, y=get_dataset(dataset_name)
-st.write("shape of dataset",X.shape)
-st.write("Number of classes", len(np.unique(y)))
+def get_dataset(name):
+    data = None
+    if name == 'Iris':
+        data = datasets.load_iris()
+    elif name == 'Wine':
+        data = datasets.load_wine()
+    else:
+        data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+    return X, y
+
+X, y = get_dataset(dataset_name)
+st.write('Shape of dataset:', X.shape)
+st.write('number of classes:', len(np.unique(y)))
 
 def add_parameter_ui(clf_name):
-	params=dict()
-	if clf_name=="KNN":
-		K=st.sidebar.slider("K",1,15)
-		params["K"]=K
-	elif clf_name=="SVM":
-		C=st.sidebar.slider("C",0.01,10.0)
-		params["C"]=C
-	else:
-		max_depth=st.sidebar.slider("max_depth",2,15)
-		n_estimators=st.sidebar.slider("n_estimators",1,100)
-		params["max_depth"]=max_depth
-		params["n_estimators"]=n_estimators
-	return params
+    params = dict()
+    if clf_name == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        params['C'] = C
+    elif clf_name == 'KNN':
+        K = st.sidebar.slider('K', 1, 15)
+        params['K'] = K
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
+    return params
 
-add_parameter_ui(classifier_name)
-	     
+params = add_parameter_ui(classifier_name)
 
-	
-def get_classifier(clf_name,params):
-	if clf_name=="KNN":
-		clf = KNeighborsClassifier(n_neighbors=params["K"])
-	elif clf_name=="SVM":
-		clf=SVC(C=params["C"]
-	else:
-		clf=RandomForestCalssifier(n_estimators=params["n_estimators"],max_depth=params["max_depth"],random_state=1234)
-	return clf
-			
-clf=get_classfier  (classifier_name,params)
-	
+def get_classifier(clf_name, params):
+    clf = None
+    if clf_name == 'SVM':
+        clf = SVC(C=params['C'])
+    elif clf_name == 'KNN':
+        clf = KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
+            max_depth=params['max_depth'], random_state=1234)
+    return clf
+
+clf = get_classifier(classifier_name, params)
+#### CLASSIFICATION ####
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+
+st.write(f'Classifier = {classifier_name}')
+st.write(f'Accuracy =', acc)
+
+#### PLOT DATASET ####
+# Project the data onto the 2 primary principal components
+pca = PCA(2)
+X_projected = pca.fit_transform(X)
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+fig = plt.figure()
+plt.scatter(x1, x2,
+        c=y, alpha=0.8,
+        cmap='viridis')
+
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.colorbar()
+
+#plt.show()
+st.pyplot(fig)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
