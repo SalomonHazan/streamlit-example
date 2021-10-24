006b186456eef124307990935a07617c66308162
Salomon Hazan <48911684+SalomonHazan@users.noreply.github.com>
2021-10-24 11:55:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,15 @@ import matplotlib as pl
 st.title('Machine Learning... The Streamlit Way')
 
 st.write("""
-# Explorer différents Classifier
+# Explorer différents modeles de classification
 Quel est le meilleur modèle ?
 """)
 
 dataset_name=st.sidebar.selectbox("Selectionner un Dataset",("Iris", "Breast Cancer", "Wine dataset"))
 st.write(dataset_name)
+
+classifier=st.sidebar.selectbox("Selectionner un modèle de classification",("KNN", "SVM", "Random Forest"))
+
 
 
 	     
